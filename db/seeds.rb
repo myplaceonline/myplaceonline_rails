a101e8a73d3b7114ff1ec3b57b3b4caeb0f34a77
Kevin Grigorenko <kevin@myplaceonline.com>
2014-10-24 22:04:52 -0700

    Move work files to order

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
-Category.create(name: "joy", link: "joy", position: 0)
-Category.create(name: "work", link: "work", position: 1)
+Category.create(name: "order", link: "order", position: 0)
+Category.create(name: "joy", link: "joy", position: 1)
 Category.create(name: "meaning", link: "meaning", position: 2)
