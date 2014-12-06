74b318739296065a22e11df16b9af111831e44cc
Kevin Grigorenko <kevin@myplaceonline.com>
2014-12-05 19:42:26 -0800

    Don't initialize devise for rake because it can't get the secret key envar

--- config/routes.rb
+++ config/routes.rb
@@ -27,26 +27,28 @@ Rails.application.routes.draw do
   post 'passwords/new'
   get 'passwords', :to => 'passwords#index'
 
-  devise_scope :user do
-    match 'users/reenter', :to => 'users/sessions#reenter', via: [:get, :post]
-    post 'users/sign_up', :to => 'users/registrations#new'
-    get 'users/delete', :to => 'users/registrations#delete'
-    match 'users/password/change', :to => 'users/registrations#changepassword', via: [:get, :put]
-    match 'users/changeemail', :to => 'users/registrations#changeemail', via: [:get, :put]
-    match 'users/resetpoints', :to => 'users/registrations#resetpoints', via: [:get, :post]
-    match 'users/advanced', :to => 'users/registrations#advanced', via: [:get, :post]
-    match 'users/deletecategory', :to => 'users/registrations#deletecategory', via: [:get, :post]
-    match 'users/security', :to => 'users/registrations#security', via: [:get, :post]
-  end
+  if defined?(Rails::Server)
+    devise_scope :user do
+      match 'users/reenter', :to => 'users/sessions#reenter', via: [:get, :post]
+      post 'users/sign_up', :to => 'users/registrations#new'
+      get 'users/delete', :to => 'users/registrations#delete'
+      match 'users/password/change', :to => 'users/registrations#changepassword', via: [:get, :put]
+      match 'users/changeemail', :to => 'users/registrations#changeemail', via: [:get, :put]
+      match 'users/resetpoints', :to => 'users/registrations#resetpoints', via: [:get, :post]
+      match 'users/advanced', :to => 'users/registrations#advanced', via: [:get, :post]
+      match 'users/deletecategory', :to => 'users/registrations#deletecategory', via: [:get, :post]
+      match 'users/security', :to => 'users/registrations#security', via: [:get, :post]
+    end
 
-  devise_for :users, controllers: {
-    confirmations: 'users/confirmations',
-    #omniauth_callbacks: 'users/omniauth_callbacks',
-    passwords: 'users/passwords',
-    registrations: 'users/registrations',
-    sessions: 'users/sessions',
-    unlocks: 'users/unlocks'
-  }
+    devise_for :users, controllers: {
+      confirmations: 'users/confirmations',
+      #omniauth_callbacks: 'users/omniauth_callbacks',
+      passwords: 'users/passwords',
+      registrations: 'users/registrations',
+      sessions: 'users/sessions',
+      unlocks: 'users/unlocks'
+    }
+  end
 
   root 'welcome#index'
 end
