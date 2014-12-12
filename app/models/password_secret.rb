71577f52a5bda34c1503282f2a8b1243cb153f27
Kevin Grigorenko <kevin@myplaceonline.com>
2014-12-12 00:06:46 -0800

    Support decrypted password answers

--- app/models/password_secret.rb
+++ app/models/password_secret.rb
@@ -3,6 +3,10 @@ class PasswordSecret < ActiveRecord::Base
   belongs_to :encrypted_answer, class_name: EncryptedValue, dependent: :destroy
   
   def getAnswer(session)
-    Myp.decrypt_from_session(session, encrypted_answer)
+    if !is_encrypted_answer
+      answer
+    else
+      Myp.decrypt_from_session(session, encrypted_answer)
+    end
   end
 end
