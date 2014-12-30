4b8bb8ba9f46857394962349554f799e3998898a
Kevin Grigorenko <kevin@myplaceonline.com>
2014-12-29 19:45:08 -0800

    Add movies to json export

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -3,12 +3,14 @@ class Identity < ActiveRecord::Base
   has_many :passwords, :dependent => :destroy
   has_many :identity_files, :dependent => :destroy
   has_many :category_points_amounts, :dependent => :destroy
+  has_many :movies, :dependent => :destroy
   
   def as_json(options={})
     super.as_json(options).merge({
+      :category_points_amounts => category_points_amounts.to_a.map{|x| x.as_json},
       :passwords => passwords.to_a.sort{ |a,b| a.name.downcase <=> b.name.downcase }.map{|x| x.as_json},
-      :identity_files => identity_files.to_a.map{|x| x.as_json},
-      :category_points_amounts => category_points_amounts.to_a.map{|x| x.as_json}
+      :movies => movies.to_a.sort{ |a,b| a.name.downcase <=> b.name.downcase }.map{|x| x.as_json},
+      :identity_files => identity_files.to_a.map{|x| x.as_json}
     })
   end
 end
