b074b6bf8c291c28537e55cfa3e1875f4e68f874
Kevin Grigorenko <kevin@myplaceonline.com>
2015-01-04 00:11:20 -0800

    Fix tests

--- test/controllers/accomplishments_controller_test.rb
+++ test/controllers/accomplishments_controller_test.rb
@@ -1,49 +1,13 @@
 require 'test_helper'
 
 class AccomplishmentsControllerTest < ActionController::TestCase
-  setup do
-    @accomplishment = accomplishments(:one)
+  include MyplaceonlineControllerTest
+  
+  def model
+    Accomplishment
   end
-
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:accomplishments)
-  end
-
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should create accomplishment" do
-    assert_difference('Accomplishment.count') do
-      post :create, accomplishment: { accomplishment: @accomplishment.accomplishment, identity_id: @accomplishment.identity_id, name: @accomplishment.name }
-    end
-
-    assert_redirected_to accomplishment_path(assigns(:accomplishment))
-  end
-
-  test "should show accomplishment" do
-    get :show, id: @accomplishment
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @accomplishment
-    assert_response :success
-  end
-
-  test "should update accomplishment" do
-    patch :update, id: @accomplishment, accomplishment: { accomplishment: @accomplishment.accomplishment, identity_id: @accomplishment.identity_id, name: @accomplishment.name }
-    assert_redirected_to accomplishment_path(assigns(:accomplishment))
-  end
-
-  test "should destroy accomplishment" do
-    assert_difference('Accomplishment.count', -1) do
-      delete :destroy, id: @accomplishment
-    end
-
-    assert_redirected_to accomplishments_path
+  
+  def test_attributes
+    { name: "test" }
   end
 end
