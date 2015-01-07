08ca669b1867dae8e32fe0910aca2af920802278
Kevin Grigorenko <kevin@myplaceonline.com>
2015-01-06 17:16:22 -0800

    Apartment.as_json returns location

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -3,4 +3,10 @@ class Apartment < ActiveRecord::Base
   belongs_to :identity
   validates_presence_of :location
   accepts_nested_attributes_for :location
+  
+  def as_json(options={})
+    super.as_json(options).merge({
+      :location => location.as_json
+    })
+  end
 end
