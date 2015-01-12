b79b01a6e1b15bbc343689ad6618c2ab8ca8673d
Kevin Grigorenko <kevin@myplaceonline.com>
2015-01-12 12:54:40 -0800

    Add Apartment.notes

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -15,9 +15,10 @@ class ApartmentsController < MyplaceonlineController
     
     def obj_params
       params.require(:apartment).permit(
+        :notes,
         select_or_create_permit(:apartment, :location_attributes, LocationsController.param_names),
         select_or_create_permit(:apartment, :landlord_attributes, ContactsController.param_names),
-        apartment_leases_attributes: [:id, :start_date, :end_date, :monthly_rent, :moveout_fee, :deposit, :terminate_by, :notes, :_destroy]
+        apartment_leases_attributes: [:id, :start_date, :end_date, :monthly_rent, :moveout_fee, :deposit, :terminate_by, :_destroy]
       )
     end
     
