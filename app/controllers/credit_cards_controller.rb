ffdc774c26b1cee3e8d76d56b27c2c211831851d
Kevin Grigorenko <kevin@myplaceonline.com>
2015-01-29 01:26:27 -0800

    CreditCard password reference

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -13,6 +13,18 @@ class CreditCardsController < MyplaceonlineController
     end
 
     def obj_params
-      params.require(:credit_card).permit(:name, :number, :expires, :security_code)
+      params.require(:credit_card).permit(
+        :name,
+        :number,
+        :expires,
+        :security_code,
+        select_or_create_permit(:credit_card, :password_attributes, PasswordsController.param_names)
+      )
+    end
+    
+    def create_presave
+      if !@obj.password.nil?
+        @obj.password.identity = current_user.primary_identity
+      end
     end
 end
--- app/models/credit_card.rb
+++ app/models/credit_card.rb
@@ -1,8 +1,19 @@
 class CreditCard < ActiveRecord::Base
-  belongs_to :password
   belongs_to :identity
   validates :name, presence: true
   validates :number, presence: true
   validates :expires, presence: true
   validates :security_code, presence: true
+  
+  belongs_to :password
+  accepts_nested_attributes_for :password, reject_if: :all_blank
+  
+  # http://stackoverflow.com/a/12064875/4135310
+  def password_attributes=(attributes)
+    if !attributes['id'].blank?
+      attributes.keep_if {|innerkey, innervalue| innerkey == "id" }
+      self.password = Password.find(attributes['id'])
+    end
+    super
+  end
 end
