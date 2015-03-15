ba75e1ce932572045e16dae1c3a6dcf473d0b204
Kevin Grigorenko <kevin@myplaceonline.com>
2015-03-15 07:55:28 -0700

    Don't require use of CalculationElement in CalculationForm

--- app/models/calculation_form.rb
+++ app/models/calculation_form.rb
@@ -7,7 +7,7 @@ class CalculationForm < ActiveRecord::Base
   accepts_nested_attributes_for :calculation_inputs, allow_destroy: true, reject_if: :all_blank
   
   belongs_to :root_element, class_name: CalculationElement, autosave: true
-  validates_associated :root_element
-  validates_presence_of :root_element
+  #validates_associated :root_element
+  #validates_presence_of :root_element
   accepts_nested_attributes_for :root_element
 end
