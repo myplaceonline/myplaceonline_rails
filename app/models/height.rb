4ceb1dfeb8126d0bc837e601f20629dc75498199
Kevin Grigorenko <kevin@myplaceonline.com>
2015-04-20 20:42:29 -0700

    Show Height in feet and inches

--- app/models/height.rb
+++ app/models/height.rb
@@ -6,9 +6,14 @@ class Height < ActiveRecord::Base
   validates :measurement_date, presence: true
   
   def display
-    result = height_amount.to_s
+    result = ""
     if amount_type == 0
-      result += " inches"
+      result += (height_amount / 12).floor.to_s + " feet"
+      inches = (height_amount % 12)
+      inches = (inches * 10**2).round.to_f / 10**2
+      if inches > 0
+        result += ", " + inches.to_s.gsub("\.0", "") + " inches"
+      end
     end
     result += " (" + Myp.display_date(measurement_date, User.current_user) + ")"
     result
