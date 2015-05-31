3e398da874d7fb7c922866e461cbcc062e177e4a
Kevin Grigorenko <kevin@myplaceonline.com>
2015-05-31 12:51:31 -0700

    EncryptedValue doesn't have an identity

--- app/models/encrypted_value.rb
+++ app/models/encrypted_value.rb
@@ -14,11 +14,4 @@ class EncryptedValue < ActiveRecord::Base
       :encryption_type => encryption_type
     }
   end
-  
-  before_create :do_before_save
-  before_update :do_before_save
-
-  def do_before_save
-    Myp.set_common_model_properties(self)
-  end
 end
