7338d15702ecdd5b7f62c76dc2220a67f846ad9d
Kevin Grigorenko <kevin@myplaceonline.com>
2015-06-10 14:54:47 -0700

    Show categories as series1 source selection

--- app/controllers/graph_controller.rb
+++ app/controllers/graph_controller.rb
@@ -2,5 +2,6 @@ class GraphController < MyplaceonlineController
   skip_authorization_check :only => MyplaceonlineController::DEFAULT_SKIP_AUTHORIZATION_CHECK + [:display]
 
   def display
+    @sources = Myp.categories.map{|k,v| I18n.t("myplaceonline.category." + v.name) }.sort
   end
 end
