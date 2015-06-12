97d0948560438696acf0914bf8572b4f18d51b78
Kevin Grigorenko <kevin@myplaceonline.com>
2015-06-12 09:01:54 -0700

    Don't require yelp API keys

--- config/initializers/yelp.rb
+++ config/initializers/yelp.rb
@@ -1,6 +1,10 @@
-Yelp.client.configure do |config|
-  config.consumer_key = ENV['YELP_CONSUMER_KEY']
-  config.consumer_secret = ENV['YELP_CONSUMER_SECRET']
-  config.token = ENV['YELP_TOKEN']
-  config.token_secret = ENV['YELP_TOKEN_SECRET']
+if !ENV['YELP_CONSUMER_KEY'].nil?
+  Yelp.client.configure do |config|
+    config.consumer_key = ENV['YELP_CONSUMER_KEY']
+    config.consumer_secret = ENV['YELP_CONSUMER_SECRET']
+    config.token = ENV['YELP_TOKEN']
+    config.token_secret = ENV['YELP_TOKEN_SECRET']
+  end
+else
+  puts "Warning: Yelp API keys not configured: YELP_CONSUMER_KEY=\"\" YELP_CONSUMER_SECRET=\"\" YELP_TOKEN=\"\" YELP_TOKEN_SECRET=\"\""
 end
