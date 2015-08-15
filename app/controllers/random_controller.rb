ac1d84ab33754e49de30c9b410f70b49ad8f84bd
Kevin Grigorenko <kevin@myplaceonline.com>
2015-08-14 18:32:59 -0700

    Add websites, movies, and books to random activity candidates

--- app/controllers/random_controller.rb
+++ app/controllers/random_controller.rb
@@ -5,15 +5,17 @@ class RandomController < MyplaceonlineController
   end
 
   def activity
-    if request.method != "GET"
-      candidates = Array.new
-      candidates = candidates + current_user.primary_identity.activities.to_a
-      if candidates.length > 0
-        @result = candidates[rand(candidates.length)]
-        category_name = @result.class.name.pluralize.underscore
-        @category = Myp.categories(User.current_user)[category_name.to_sym]
-        @result_link = "/" + category_name + "/" + @result.id.to_s
-      end
+    identity = current_user.primary_identity
+    candidates = Array.new
+    candidates = candidates + identity.activities.to_a
+    candidates = candidates + identity.websites.to_a
+    candidates = candidates + identity.movies.to_a
+    candidates = candidates + identity.books.to_a
+    if candidates.length > 0
+      @result = candidates[rand(candidates.length)]
+      category_name = @result.class.name.pluralize.underscore
+      @category = Myp.categories(User.current_user)[category_name.to_sym]
+      @result_link = "/" + category_name + "/" + @result.id.to_s
     end
   end
 end
