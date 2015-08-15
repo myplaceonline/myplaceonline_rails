4bedd49977b2c0ad3ac143604d005c19a5b7eb35
Kevin Grigorenko <kevin@myplaceonline.com>
2015-08-14 18:29:50 -0700

    Add basics of random activity

--- app/controllers/random_controller.rb
+++ app/controllers/random_controller.rb
@@ -5,5 +5,15 @@ class RandomController < MyplaceonlineController
   end
 
   def activity
+    if request.method != "GET"
+      candidates = Array.new
+      candidates = candidates + current_user.primary_identity.activities.to_a
+      if candidates.length > 0
+        @result = candidates[rand(candidates.length)]
+        category_name = @result.class.name.pluralize.underscore
+        @category = Myp.categories(User.current_user)[category_name.to_sym]
+        @result_link = "/" + category_name + "/" + @result.id.to_s
+      end
+    end
   end
 end
