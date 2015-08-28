4c131e0675d91a11e78255c76ff95a72e37a3e58
Kevin Grigorenko <kevin@myplaceonline.com>
2015-08-27 19:45:50 -0700

    Don't show expired passports by default

--- app/controllers/passports_controller.rb
+++ app/controllers/passports_controller.rb
@@ -3,7 +3,25 @@ class PassportsController < MyplaceonlineController
     true
   end
 
+  def index
+    @expired = params[:expired]
+    if !@expired.blank?
+      @expired = @expired.to_bool
+    end
+    super
+  end
+
   protected
+    def all
+      if @expired.blank? || !@expired
+        model.where("owner_id = ? and (expires is null or expires > now())", current_user.primary_identity)
+      else
+        model.where(
+          owner_id: current_user.primary_identity.id
+        )
+      end
+    end
+
     def sorts
       ["lower(passports.region) ASC"]
     end
