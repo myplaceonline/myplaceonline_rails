d032ad085165e3f1d795a1c3bd14bb586db7d9b0
Kevin Grigorenko <kevin@myplaceonline.com>
2015-09-06 15:53:42 -0700

    Create function that builds default myplets

--- app/models/myplet.rb
+++ app/models/myplet.rb
@@ -4,9 +4,71 @@ class Myplet < MyplaceonlineIdentityRecord
     nil
   end
   
-  def self.default_myplets
+  def self.default_myplets(identity)
     result = Array.new
-    result.push(Myplet.new({y_coordinate: 0, x_coordinate: 0, title: ""}))
+
+    x = PointDisplay.new
+    x.save!
+    result.push(Myplet.new({
+      title: "myplaceonline.myplets.titles.point_display",
+      y_coordinate: 0,
+      x_coordinate: 0,
+      category_name: x.class.name.underscore.pluralize,
+      category_id: x.id,
+      border_type: 0,
+      owner: identity
+    }))
+
+    x = MyplaceonlineSearch.new
+    x.save!
+    result.push(Myplet.new({
+      title: "myplaceonline.myplets.titles.myplaceonline_search",
+      y_coordinate: 1,
+      x_coordinate: 0,
+      category_name: x.class.name.underscore.pluralize,
+      category_id: x.id,
+      border_type: 1,
+      owner: identity
+    }))
+
+    x = MyplaceonlineQuickCategoryDisplay.new
+    x.save!
+    result.push(Myplet.new({
+      title: "myplaceonline.myplets.titles.myplaceonline_quick_category_display",
+      y_coordinate: 2,
+      x_coordinate: 0,
+      category_name: x.class.name.underscore.pluralize,
+      category_id: x.id,
+      border_type: 1,
+      owner: identity
+    }))
+
+    x = MyplaceonlineDueDisplay.new
+    x.save!
+    result.push(Myplet.new({
+      title: "myplaceonline.myplets.titles.myplaceonline_due_display",
+      y_coordinate: 3,
+      x_coordinate: 0,
+      category_name: x.class.name.underscore.pluralize,
+      category_id: x.id,
+      border_type: 1,
+      owner: identity
+    }))
+
+    x = Notepad.new({
+      title: I18n.t("myplaceonline.myplets.titles.notepad")
+    })
+    x.save!
+    result.push(Myplet.new({
+      title: "myplaceonline.myplets.titles.notepad",
+      y_coordinate: 4,
+      x_coordinate: 0,
+      category_name: x.class.name.underscore.pluralize,
+      category_id: x.id,
+      border_type: 1,
+      owner: identity
+    }))
+
     result
   end
 end
