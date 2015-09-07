b5cdebb9aad03d32f8c5befda9a6d82afd435511
Kevin Grigorenko <kevin@myplaceonline.com>
2015-09-07 16:54:25 -0700

    Fix UnknownFormat error from GoogleBot

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  respond_to :html, :json
+  
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # TODO broken with JQueryMobile on login/logout
