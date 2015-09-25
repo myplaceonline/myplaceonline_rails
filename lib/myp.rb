24af6300f6898009e289e2b74397ce55f4ba4fb4
Kevin Grigorenko <kevin@myplaceonline.com>
2015-09-24 21:47:50 -0700

    Add screenshots to homepage

--- lib/myp.rb
+++ lib/myp.rb
@@ -273,7 +273,13 @@ module Myp
     end
   end
   
-  WELCOME_FEATURES = self.parse_yaml_to_html("myplaceonline.welcome.features")
+  WELCOME_FEATURES = self.parse_yaml_to_html("myplaceonline.welcome.features") % {
+    :screenshot1 => ActionController::Base.helpers.image_tag("screenshot1.png"),
+    :screenshot2 => ActionController::Base.helpers.image_tag("screenshot6.png"),
+    :screenshot3 => ActionController::Base.helpers.image_tag("screenshot7.png"),
+    :screenshot4 => ActionController::Base.helpers.image_tag("screenshot8.png"),
+    :feature_details => self.parse_yaml_to_html("myplaceonline.welcome.feature_details")
+  }
   CONTENT_FAQ = self.parse_yaml_to_html("myplaceonline.info.faq_content")
   CONTENT_TIPS = self.parse_yaml_to_html("myplaceonline.info.tips_content")
   
