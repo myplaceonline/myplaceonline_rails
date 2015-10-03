20fc70a25feb8ac813f547f68a3f97fa9e0160b3
Kevin Grigorenko <kevin@myplaceonline.com>
2015-10-03 09:58:02 -0700

    Add agree to terms checkbox to sign up

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -24,32 +24,39 @@ class Users::RegistrationsController < Devise::RegistrationsController
   before_filter :configure_permitted_parameters
   
   def new
+    @agree_terms = params[:agree_terms]
     if request.post?
       build_resource(sign_up_params)
-
-      resource_saved = resource.save
-      yield resource if block_given?
-      if resource_saved
-        
-        Myp.remember_password(session, params[:user][:password])
-        
-        if resource.active_for_authentication?
-          set_flash_message :notice, :signed_up if is_flashing_format?
-          sign_up(resource_name, resource)
-          #respond_with resource, location: after_sign_up_path_for(resource)
-          redirect_to after_sign_up_path_for(resource)
+      
+      if @agree_terms == "1"
+        resource_saved = resource.save
+        yield resource if block_given?
+        if resource_saved
+          
+          Myp.remember_password(session, params[:user][:password])
+          
+          if resource.active_for_authentication?
+            set_flash_message :notice, :signed_up if is_flashing_format?
+            sign_up(resource_name, resource)
+            #respond_with resource, location: after_sign_up_path_for(resource)
+            redirect_to after_sign_up_path_for(resource)
+          else
+            set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
+            expire_data_after_sign_in!
+            #respond_with resource, location: after_inactive_sign_up_path_for(resource)
+            redirect_to after_inactive_sign_up_path_for(resource)
+          end
         else
-          set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
-          expire_data_after_sign_in!
-          #respond_with resource, location: after_inactive_sign_up_path_for(resource)
-          redirect_to after_inactive_sign_up_path_for(resource)
+          clean_up_passwords resource
+          @validatable = devise_mapping.validatable?
+          if @validatable
+            @minimum_password_length = resource_class.password_length.min
+          end
+          respond_with resource
         end
       else
+        flash[:error] = t("myplaceonline.users.must_agree_terms", :terms => t("myplaceonline.info.terms"))
         clean_up_passwords resource
-        @validatable = devise_mapping.validatable?
-        if @validatable
-          @minimum_password_length = resource_class.password_length.min
-        end
         respond_with resource
       end
     else
