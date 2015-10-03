90180db8cd23b98dd6132545c4851ccf350efb10
Kevin Grigorenko <kevin@myplaceonline.com>
2015-10-03 15:15:49 -0700

    Add apartment trash pickup next pickup method

--- app/models/apartment_trash_pickup.rb
+++ app/models/apartment_trash_pickup.rb
@@ -10,4 +10,20 @@ class ApartmentTrashPickup < MyplaceonlineIdentityRecord
   validates :start_date, presence: true
   validates :period_type, presence: true
   validates :period, presence: true
+  
+  def next_pickup
+    # Start at the start date, and keep adding the period until we're >= today
+    result = start_date
+    today = Date.today
+    while result < today
+      if period_type == 0
+        result = result.advance(days: period)
+      elsif period_type == 1
+        result = result.advance(weeks: period)
+      else
+        result = result.advance(weeks: 4*period)
+      end
+    end
+    result
+  end
 end
