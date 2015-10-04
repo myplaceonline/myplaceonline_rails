59d9c325c091711cb0f0f7036fc14c389c4d5783
Kevin Grigorenko <kevin@myplaceonline.com>
2015-10-04 13:21:01 -0700

    Don't automatically convert links to https

--- app/models/password.rb
+++ app/models/password.rb
@@ -28,11 +28,11 @@ class Password < MyplaceonlineIdentityRecord
     result = url
     if !result.to_s.empty?
       if prefertls && result.start_with?("http:")
-        result = "https" + result[4..-1]
+        #result = "https" + result[4..-1]
       end
       
       if (/^[^:]+:.*/ =~ result).nil?
-        result = (prefertls ? "https://" : "http://") + result
+        #result = (prefertls ? "https://" : "http://") + result
       end
     end
     result
