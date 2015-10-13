979f5c9ddb02a2a5f1fd091c2540cbdb26926929
Kevin Grigorenko <kevin@myplaceonline.com>
2015-10-12 21:29:26 -0700

    Don't update visit count when showing myplet on homepage

--- app/controllers/myplaceonline_controller.rb
+++ app/controllers/myplaceonline_controller.rb
@@ -321,11 +321,13 @@ class MyplaceonlineController < ApplicationController
     
     def before_show
       # Use update_column because we don't want updated_at to be updated
-      if @obj.respond_to?("visit_count")
-        if @obj.visit_count?
-          @obj.update_column(:visit_count, @obj.visit_count + 1)
-        else
-          @obj.update_column(:visit_count, 1)
+      if params[:myplet].nil?
+        if @obj.respond_to?("visit_count")
+          if @obj.visit_count?
+            @obj.update_column(:visit_count, @obj.visit_count + 1)
+          else
+            @obj.update_column(:visit_count, 1)
+          end
         end
       end
     end
