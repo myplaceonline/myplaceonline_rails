0d9745175bf13bd13a08727ee474a193329e0501
Kevin Grigorenko <kevin@myplaceonline.com>
2015-12-14 23:54:35 -0800

    Update some production configuration for Rails 4.2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = true
+  config.serve_static_files = true
 
   # Compress JavaScripts and CSS.
   # config.assets.js_compressor = :uglifier
@@ -43,7 +43,7 @@ Rails.application.configure do
   #config.force_ssl = true
 
   # Set to :debug to see everything in the log.
-  #config.log_level = :debug
+  config.log_level = :info
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
