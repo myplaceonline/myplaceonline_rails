010c50c204190876d1d175a9863ffb15b1e1e7e3
Kevin Grigorenko <kevin@myplaceonline.com>
2015-12-17 10:05:17 -0800

    Remove default due time from TODOs

--- app/models/to_do.rb
+++ app/models/to_do.rb
@@ -7,12 +7,6 @@ class ToDo < ActiveRecord::Base
     short_description
   end
   
-  def self.build(params = nil)
-    result = self.dobuild(params)
-    result.due_time = DateTime.now
-    result
-  end
-  
   after_save { |record| DueItem.due_todos(User.current_user, record, DueItem::UPDATE_TYPE_UPDATE) }
   after_destroy { |record| DueItem.due_todos(User.current_user, record, DueItem::UPDATE_TYPE_DELETE) }
 end
