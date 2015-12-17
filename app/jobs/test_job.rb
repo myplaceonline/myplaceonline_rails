f85cf0840ceb24710490fc2706a0550e836bc5d2
Kevin Grigorenko <kevin@myplaceonline.com>
2015-12-17 07:36:12 -0800

    Add debug to test job

--- app/jobs/test_job.rb
+++ app/jobs/test_job.rb
@@ -4,6 +4,8 @@ class TestJob < ApplicationJob
   def perform(*args)
     Rails.logger.debug{"Started TestJob arg1=#{args[0]}"}
     Rails.logger.debug{"default_url_options = #{default_url_options.inspect}"}
+    test = playlists_url
+    Rails.logger.debug{"test = #{test.inspect}"}
     Rails.logger.debug{"Ended TestJob"}
   end
 end
