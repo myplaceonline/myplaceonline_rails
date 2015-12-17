856ed47abd4b575a5717c90604da15f3d743c9cf
Kevin Grigorenko <kevin@myplaceonline.com>
2015-12-17 07:41:21 -0800

    Add test job debug

--- app/jobs/test_job.rb
+++ app/jobs/test_job.rb
@@ -2,10 +2,10 @@ class TestJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    Rails.logger.debug{"Started TestJob arg1=#{args[0]}"}
-    Rails.logger.debug{"default_url_options = #{default_url_options.inspect}"}
+    Rails.logger.info{"Started TestJob arg1=#{args[0]}"}
+    Rails.logger.info{"default_url_options = #{default_url_options.inspect}"}
     test = playlists_url
-    Rails.logger.debug{"test = #{test.inspect}"}
-    Rails.logger.debug{"Ended TestJob"}
+    Rails.logger.info{"test = #{test.inspect}"}
+    Rails.logger.info{"Ended TestJob"}
   end
 end
