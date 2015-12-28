e8b3136fb8b85d56fc8a02aea4c89d3a6487cd60
Kevin Grigorenko <kevin@myplaceonline.com>
2015-12-27 21:24:11 -0800

    Add debug statement for tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,9 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
+  
+  puts "Myplaceonline tests started"
+  
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
