f44554a85491d67033d5dd93c75e3dda302c7835
Kevin Grigorenko <kevin@myplaceonline.com>
2016-01-11 09:51:21 -0800

    Allow movies and books without recommender

--- app/models/book.rb
+++ app/models/book.rb
@@ -8,7 +8,7 @@ class Book < ActiveRecord::Base
   boolean_time_transfer :is_read, :when_read
   
   belongs_to :recommender, class_name: Contact, :autosave => true
-  accepts_nested_attributes_for :recommender, reject_if: :all_blank
+  accepts_nested_attributes_for :recommender, reject_if: proc { |attributes| ContactsController.reject_if_blank(attributes) }
   allow_existing :recommender, Contact
   
   def display
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -8,7 +8,7 @@ class Movie < ActiveRecord::Base
   boolean_time_transfer :is_watched, :watched
   
   belongs_to :recommender, class_name: Contact, :autosave => true
-  accepts_nested_attributes_for :recommender, reject_if: :all_blank
+  accepts_nested_attributes_for :recommender, reject_if: proc { |attributes| ContactsController.reject_if_blank(attributes) }
   allow_existing :recommender, Contact
 
   def display
