e6ce2cb147a5a405221f11cd0b71184cfd64459a
Kevin Grigorenko <kevin@myplaceonline.com>
2016-02-06 06:37:06 -0800

    Update migration

--- db/migrate/20160206143411_handle_deletion_failure.rb
+++ db/migrate/20160206143411_handle_deletion_failure.rb
@@ -1,6 +1,5 @@
 class HandleDeletionFailure < ActiveRecord::Migration
   def change
-    p = Promotion.find(16)
-    CalendarItem.destroy_calendar_items(p.identity, p.class, model_id: p.id)
+    CalendarItem.destroy_calendar_items(Identity.find(1), "Promotion", model_id: 16)
   end
 end
