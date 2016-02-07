ca95f20a02736229ae14b44cbee3e0f94000f5e4
Kevin Grigorenko <kevin@myplaceonline.com>
2016-02-06 17:59:52 -0800

    Change website required attributes

--- app/models/website.rb
+++ app/models/website.rb
@@ -2,7 +2,7 @@ class Website < ActiveRecord::Base
   include MyplaceonlineActiveRecordIdentityConcern
   include AllowExistingConcern
 
-  validates :url, presence: true
+  validates :title, presence: true
   
   has_many :website_passwords
   accepts_nested_attributes_for :website_passwords, allow_destroy: true, reject_if: :all_blank
