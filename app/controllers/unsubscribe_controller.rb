eff89617bdb10e6d90e21f9e4001866e41ce0d25
Kevin Grigorenko <kevin@myplaceonline.com>
2016-02-26 22:47:51 -0800

    Add unsubscribe handlers

--- app/controllers/unsubscribe_controller.rb
+++ app/controllers/unsubscribe_controller.rb
@@ -3,5 +3,28 @@ class UnsubscribeController < ApplicationController
   skip_authorization_check
   
   def index
+    email = params[:email]
+    category = params[:category]
+    token = params[:token]
+    
+    @content = ""
+    
+    if !EmailToken.where(email: email, token: token).first.nil?
+      flash.clear
+      
+      eu = EmailUnsubscription.new
+      eu.email = email
+      eu.category = category
+      eu.save!
+      
+      if category.blank?
+        @content = t("myplaceonline.unsubscribe.unsubscribed_all")
+      else
+        @content = t("myplaceonline.unsubscribe.unsubscribed_category", {category: category})
+      end
+      
+    else
+      flash[:error] = t("myplaceonline.unsubscribe.invalid_token")
+    end
   end
 end
