5ea758b19034f523041bfcb8457251c31e714996
Kevin Grigorenko <kevin@myplaceonline.com>
2016-03-07 02:02:18 -0800

    Debu

--- app/jobs/share_job.rb
+++ app/jobs/share_job.rb
@@ -10,6 +10,7 @@ class ShareJob < ApplicationJob
   def perform(*args)
     Rails.logger.debug{"Started ShareJob"}
     permission_share = args[0]
+    Rails.logger.info{"ShareJob permission_share: #{permission_share.inspect}"}
     permission_share.execute_share
     Rails.logger.debug{"Finished ShareJob"}
   end
