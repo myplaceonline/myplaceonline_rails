cdbcce94d24abcd988cf0da193e694d926fc2af4
Kevin Grigorenko <kevin@myplaceonline.com>
2016-03-09 17:52:54 -0800

    Re-initialize apartment trash pickup reminders to take into account max_pending

---
+++ db/migrate/20160310015220_update_apartment_trash_pickups_max_pendings.rb
@@ -0,0 +1,8 @@
+class UpdateApartmentTrashPickupsMaxPendings < ActiveRecord::Migration
+  def change
+    ApartmentTrashPickup.all.each do |x|
+      User.current_user = x.identity.user
+      x.on_after_save
+    end
+  end
+end
