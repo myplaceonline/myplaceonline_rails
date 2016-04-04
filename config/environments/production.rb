d6068776c08d2df5c2731585926c207e122fe901
Kevin Grigorenko <kevin@myplaceonline.com>
2016-04-03 20:11:59 -0700

    Force SSL in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,8 +39,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # We do this already in nginx
-  #config.force_ssl = true
+  config.force_ssl = true
 
   # Set to :debug to see everything in the log.
   config.log_level = :info
