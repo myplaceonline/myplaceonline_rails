ef565eb5c2a33cc711002edc59808e4e62a9a559
Kevin Grigorenko <kevin@myplaceonline.com>
2016-05-26 08:22:23 -0700

    Use generic syslog name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,7 @@ Rails.application.configure do
 
   # Use a different logger for distributed setups.
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
-  config.logger = Syslog::Logger.new "myplaceonline-rails"
+  config.logger = Syslog::Logger.new "rails"
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
