4fd9d085a14257a341164058f0a335b20e5a5867
Kevin Grigorenko <kevin@myplaceonline.com>
2016-08-03 18:49:57 -0700

    Fix tooltop for project issues split button

--- app/controllers/project_issues_controller.rb
+++ app/controllers/project_issues_controller.rb
@@ -13,7 +13,7 @@ class ProjectIssuesController < MyplaceonlineController
   
   def split_link(obj)
     ActionController::Base.helpers.link_to(
-      I18n.t("myplaceonline.projects.project_issue_add"),
+      I18n.t("myplaceonline.project_issues.move_top"),
       project_project_issues_movetop_path(obj.project, obj)
     )
   end
