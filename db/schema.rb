1e98ae18d303e0bca265cb82fa49f239d00b54e8
Kevin Grigorenko <kevin@myplaceonline.com>
2016-08-15 11:12:39 -0700

    Add a request wrapper RailTie

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160815073213) do
+ActiveRecord::Schema.define(version: 20160815181052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
