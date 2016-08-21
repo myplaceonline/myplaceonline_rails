4628ac2a0cde10a610fb68feaa023bcd2cfe41df
Kevin Grigorenko <kevin@myplaceonline.com>
2016-08-21 01:52:16 -0700

    Fix bad timezone error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -113,7 +113,11 @@ class ApplicationController < ActionController::Base
   
   def set_time_zone
     if !current_user.nil? && !current_user.timezone.blank?
-      Time.zone = current_user.timezone
+      begin
+        Time.zone = current_user.timezone
+      rescue Exception => e
+        Myp.warn("Invalid time zone #{Myp.error_details(e)}")
+      end
     end
   end
 
