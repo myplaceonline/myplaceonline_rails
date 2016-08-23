5a8e87f471feee8b393305b7982a4b42a0d628a7
Kevin Grigorenko <kevin@myplaceonline.com>
2016-08-22 17:02:27 -0700

    Support contact emails for full text search results

--- app/models/identity_email.rb
+++ app/models/identity_email.rb
@@ -2,4 +2,8 @@ class IdentityEmail < ActiveRecord::Base
   include MyplaceonlineActiveRecordIdentityConcern
 
   belongs_to :parent_identity, class_name: Identity
+  
+  def final_search_result
+    parent_identity.contact
+  end
 end
