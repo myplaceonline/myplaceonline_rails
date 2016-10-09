1bd5f8221b734a2457f7d00b75c7d6e5cf6080e2
Kevin Grigorenko <kevin@myplaceonline.com>
2016-10-09 13:49:06 -0700

    Sort life highlights by time and show time in bubble

--- app/controllers/life_highlights_controller.rb
+++ app/controllers/life_highlights_controller.rb
@@ -1,11 +1,19 @@
 class LifeHighlightsController < MyplaceonlineController
+  def use_bubble?
+    true
+  end
+  
+  def bubble_text(obj)
+    Myp.display_date_month_year(obj.life_highlight_time, User.current_user)
+  end
+    
   protected
     def insecure
       true
     end
 
     def sorts
-      ["lower(life_highlights.life_highlight_name) ASC"]
+      ["life_highlights.life_highlight_time DESC", "lower(life_highlights.life_highlight_name) ASC"]
     end
 
     def obj_params
