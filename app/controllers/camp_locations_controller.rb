1b4c011ab41252ee588dd723b6e34a2ca2e5c9a0
Kevin Grigorenko <kevin@myplaceonline.com>
2016-11-13 09:58:06 -0800

    Sort camp locations by location name

--- app/controllers/camp_locations_controller.rb
+++ app/controllers/camp_locations_controller.rb
@@ -8,10 +8,6 @@ class CampLocationsController < MyplaceonlineController
   end
 
   protected
-    def sorts
-      ["camp_locations.updated_at DESC"]
-    end
-
     def obj_params
       params.require(:camp_location).permit(
         :notes,
@@ -33,5 +29,27 @@ class CampLocationsController < MyplaceonlineController
         location_attributes: LocationsController.param_names,
         membership_attributes: MembershipsController.param_names
       )
+    end
+
+    def sorts
+      [%{
+       CASE
+        WHEN locations.name IS NOT NULL THEN locations.name
+        WHEN locations.address1 IS NOT NULL THEN locations.address1
+        WHEN locations.address2 IS NOT NULL THEN locations.address2
+        WHEN locations.address3 IS NOT NULL THEN locations.address3
+        WHEN locations.sub_region2 IS NOT NULL THEN locations.sub_region2
+        WHEN locations.sub_region1 IS NOT NULL THEN locations.sub_region1
+        ELSE locations.region
+       END ASC
+      }]
+    end
+    
+    def all_joins
+      "INNER JOIN locations ON locations.id = camp_locations.location_id"
+    end
+
+    def all_includes
+      :location
     end
 end
--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -91,10 +91,6 @@ class PodcastsController < MyplaceonlineController
   end
 
   protected
-    def sorts
-      ["podcasts.updated_at DESC"]
-    end
-
     def obj_params
       params.require(:podcast).permit(
         feed_attributes: FeedsController.param_names
