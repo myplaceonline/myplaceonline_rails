16fa900028afe1a114cfcd01fd684253fa20d63d
Kevin Grigorenko <kevin@myplaceonline.com>
2016-12-06 23:21:43 -0800

    Require ExecutionContext for ExecutionContextRequestStrategy

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,7 @@ require 'log4r'
 require 'log4r/yamlconfigurator'
 require 'log4r/outputter/datefileoutputter'
 require 'fileutils'
+require './lib/execution_context'
 include Log4r
 
 # Require the gems listed in Gemfile, including any gems
