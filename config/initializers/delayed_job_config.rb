147abadf29983769e223cf0d5a0c798e05343caa
Kevin Grigorenko <kevin@myplaceonline.com>
2016-12-08 20:07:03 -0800

    Set max attempts to 1 for delayed jobs

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -1,1 +1,10 @@
 Delayed::Worker.logger = Rails.logger
+Delayed::Worker.destroy_failed_jobs = false
+#Delayed::Worker.sleep_delay = 60
+Delayed::Worker.max_attempts = 1
+Delayed::Worker.max_run_time = 1.hours
+Delayed::Worker.read_ahead = 10
+#Delayed::Worker.default_queue_name = 'default'
+#Delayed::Worker.delay_jobs = !Rails.env.test?
+Delayed::Worker.raise_signal_exceptions = :term
+#Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'))
