6c2d57da7a5f1825c76aee91ce7bae2880314684
Kevin Grigorenko <kevin@myplaceonline.com>
2017-01-27 22:52:12 -0800

    Enhance blood concentration display

--- app/models/blood_concentration.rb
+++ app/models/blood_concentration.rb
@@ -4,7 +4,33 @@ class BloodConcentration < ApplicationRecord
   validates :concentration_name, presence: true
   
   def display
-    concentration_name
+    result = self.concentration_name
+    if self.concentration_minimum.nil? && self.concentration_maximum.nil?
+      result = Myp.appendstrwrap(result, Myp.get_select_name(self.concentration_type, Myp::LIQUID_CONCENTRATIONS))
+    elsif !self.concentration_minimum.nil? && !self.concentration_maximum.nil?
+      result = Myp.appendstrwrap(result, self.concentration_minimum_with_type + " - " + self.concentration_maximum_with_type)
+    elsif !self.concentration_minimum.nil?
+      result = Myp.appendstrwrap(result, "> " + self.concentration_minimum_with_type)
+    else
+      result = Myp.appendstrwrap(result, "< " + self.concentration_maximum_with_type)
+    end
+    result
+  end
+
+  def concentration_minimum_with_type
+    if self.concentration_minimum.nil?
+      nil
+    else
+      Myp.appendstr(self.concentration_minimum.to_s, Myp.get_select_name(self.concentration_type, Myp::LIQUID_CONCENTRATIONS))
+    end
+  end
+  
+  def concentration_maximum_with_type
+    if self.concentration_maximum.nil?
+      nil
+    else
+      Myp.appendstr(self.concentration_maximum.to_s, Myp.get_select_name(self.concentration_type, Myp::LIQUID_CONCENTRATIONS))
+    end
   end
   
   def self.params
