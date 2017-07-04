fc36a96f6c78c1f4ad60aa6a20873fecb78fea98
Kevin Grigorenko <kevin@myplaceonline.com>
2017-07-04 12:50:00 -0700

    Re-import Food Informations wrapped USDA info

---
+++ db/migrate/20170704194713_reimport_food_informations.rb
@@ -0,0 +1,16 @@
+class ReimportFoodInformations < ActiveRecord::Migration[5.1]
+  disable_ddl_transaction!
+
+  def change
+    MyplaceonlineExecutionContext.do_user(User.super_user) do
+      UsdaNutrientDatabase::Food.all.each do |food|
+        FoodInformation.create!(
+          identity_id: User.current_user.primary_identity_id,
+          food_name: food.long_description,
+          notes: food.short_description,
+          usda_food: food
+        )
+      end
+    end
+  end
+end
