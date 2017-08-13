409793bc6460e970d9d57ea1bc979549a20f3b7e
Kevin Grigorenko <kevin@myplaceonline.com>
2017-08-12 19:53:36 -0700

    Use soft ulimit

--- app/models/identity_file.rb
+++ app/models/identity_file.rb
@@ -210,7 +210,7 @@ class IdentityFile < ApplicationRecord
         # http://www.imagemagick.org/Usage/resize/
         # Ulimit is in KB
         Open3.popen3(%{
-          ulimit -v 102400; convert #{self.filesystem_path} -auto-orient -thumbnail '#{max_width}>' #{thumbnail_path}
+          ulimit -Sv 102400 && convert #{self.filesystem_path} -auto-orient -thumbnail '#{max_width}>' #{thumbnail_path}
         }) do |stdin, stdout, stderr, wait_thr|
           exit_status = wait_thr.value
           if exit_status != 0
