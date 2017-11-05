32ab9cbc0dda854b8b19c333d46c87274e989551
Kevin G <kevin@myplaceonline.com>
2017-11-05 10:56:31 -0800

    Create default myplets

---
+++ db/migrate/20171105185545_create_default_myplets2.rb
@@ -0,0 +1,8 @@
+class CreateDefaultMyplets2 < ActiveRecord::Migration[5.1]
+  def change
+    MyplaceonlineExecutionContext.do_user(User.super_user) do
+      domain = WebsiteDomain.where(identity_id: User::SUPER_USER_IDENTITY_ID, verified: true, domain_name: "myplaceonline").take!
+      Myp.create_default_website_myplets(identity_id: User::SUPER_USER_IDENTITY_ID, website_domain: domain)
+    end
+  end
+end
