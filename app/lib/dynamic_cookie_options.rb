83066debac08097ec2fdd2dee2d7488e08d40a42
Kevin G <kevin@myplaceonline.com>
2017-11-08 21:50:25 -0800

    Add debug

--- app/lib/dynamic_cookie_options.rb
+++ app/lib/dynamic_cookie_options.rb
@@ -11,11 +11,13 @@ class DynamicCookieOptions
   def self.cookie_domain
     domain = "localhost"
     if ExecutionContext.available?
+      Rails.logger.info{"DynamicCookieOptions ExecutionContext available"}
       if Rails.env.production?
         domain = Myp.top_host
+        Rails.logger.info{"DynamicCookieOptions domain: #{domain}"}
       end
     else
-      Rails.logger.info{"Cookie domain incalculable #{Myp.current_stack}"}
+      Rails.logger.info{"DynamicCookieOptions Cookie domain incalculable #{Myp.current_stack}"}
     end
     domain
   end
