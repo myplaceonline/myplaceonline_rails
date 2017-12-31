ea205b8a3bbd4b68118dab69cdac3dc5856a0c69
Kevin G <kevin@myplaceonline.com>
2017-12-31 11:47:18 -0800

    Fix user list

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171230075439) do
+ActiveRecord::Schema.define(version: 20171231194624) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -6032,6 +6032,7 @@ ActiveRecord::Schema.define(version: 20171230075439) do
     t.integer "encryption_mode"
     t.boolean "non_fixed_header"
     t.boolean "toggle_hide_footer"
+    t.datetime "archived"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
