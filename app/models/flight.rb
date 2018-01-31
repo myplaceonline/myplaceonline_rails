899783215371985044416613d7aa71963f6ca51a
Kevin G <kevin@myplaceonline.com>
2018-01-30 17:07:54 -0800

    Add flight checkin reminder

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -8,7 +8,7 @@ class Flight < ApplicationRecord
   child_property(name: :website)
 
   def display
-    Myp.appendstrwrap(flight_name, Myp.display_date_short_year(flight_start_date, User.current_user))
+    Myp.appendstrwrap(flight_name, Myp.display_datetime_short_year(flight_start_date, User.current_user))
   end
   
   def display_with_first_leg
@@ -43,5 +43,40 @@ class Flight < ApplicationRecord
     else
       nil
     end
+  end
+  
+  def earliest_checkin_time
+    24.hours
+  end
+
+  after_commit :on_after_save, on: [:create, :update]
+  
+  def on_after_save
+    ApplicationRecord.transaction do
+      self.on_after_destroy
+      if !self.flight_start_date.nil?
+        User.current_user.current_identity.calendars.each do |calendar|
+          CalendarItem.create_calendar_item(
+            identity: User.current_user.current_identity,
+            calendar: calendar,
+            model: Flight,
+            model_id: self.id,
+            calendar_item_time: self.flight_start_date,
+            reminder_threshold_amount: self.earliest_checkin_time.seconds,
+            reminder_threshold_type: Calendar::DEFAULT_REMINDER_TYPE,
+          )
+        end
+      end
+    end
+  end
+  
+  after_commit :on_after_destroy, on: :destroy
+  
+  def on_after_destroy
+    CalendarItem.destroy_calendar_items(
+      User.current_user.current_identity,
+      Flight,
+      model_id: self.id,
+    )
   end
 end
