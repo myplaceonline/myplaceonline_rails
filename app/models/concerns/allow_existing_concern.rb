57a39474b444f09763f8152668b2199b5677203f
Kevin G <kevin@myplaceonline.com>
2018-06-01 18:23:26 -0700

    Support clearing contact company

--- app/models/concerns/allow_existing_concern.rb
+++ app/models/concerns/allow_existing_concern.rb
@@ -56,7 +56,7 @@ module AllowExistingConcern extend ActiveSupport::Concern
     def set_property_with_attributes(name:, attributes:, update_type: AllowExistingConcern::UPDATE_TYPE_UNKNOWN)
       model = self.class.child_property_models[name]
       
-      Rails.logger.debug{"AllowExistingConcern.set_property_with_attributes #{self.class} name: #{name}, model: #{model}, attributes: #{Myp.debug_print(attributes)}, self: #{self.inspect}"}
+      Rails.logger.debug{"AllowExistingConcern.set_property_with_attributes #{self.class} name: #{name}, model: #{model}, attributes: #{Myp.debug_print(attributes)}, self: #{self.inspect}, update_type: #{update_type}"}
       
       set_property_modify_attributes(attributes: attributes)
       
@@ -64,66 +64,80 @@ module AllowExistingConcern extend ActiveSupport::Concern
       
       if !attributes.nil?
         if !attributes["id"].blank?
-          if !attributes["_updatetype"].blank?
-            update_type = attributes["_updatetype"].to_i
-            attributes.delete_if {|innerkey, innervalue| innerkey == "_updatetype" }
-          end
           
-          previous_object_id = self.send(name.to_s + "_id")
-          if !previous_object_id.nil?
-            previous_object_id = previous_object_id.to_s
-          end
-          
-          Rails.logger.debug{"AllowExistingConcern.set_property_with_attributes previous_object_id: #{previous_object_id}, update_type: #{update_type}"}
-          
-          previous_object = PREVIOUS_OBJECT_BLANK
-          if !previous_object_id.blank?
-            previous_object = PREVIOUS_OBJECT_DIFFERENT
-            if attributes["id"] == previous_object_id
-              previous_object = PREVIOUS_OBJECT_SAME
+          if attributes["_destroy"] != "1"
+            
+            # Even if we're not destroying, there may be a _destroy=0 param
+            # which we need to get rid of before continuing
+            attributes.delete_if {|innerkey, innervalue| innerkey == "_destroy" }
+            
+            if !attributes["_updatetype"].blank?
+              update_type = attributes["_updatetype"].to_i
+              attributes.delete_if {|innerkey, innervalue| innerkey == "_updatetype" }
             end
-          end
-          
-          # Let's first check whether the non-id attributes are set or not
-          check_attributes = attributes.dup
-          check_attributes.delete_if {|innerkey, innervalue| innerkey == "id" }
+            
+            previous_object_id = self.send(name.to_s + "_id")
+            if !previous_object_id.nil?
+              previous_object_id = previous_object_id.to_s
+            end
+            
+            Rails.logger.debug{"AllowExistingConcern.set_property_with_attributes previous_object_id: #{previous_object_id}, update_type: #{update_type}"}
+            
+            previous_object = PREVIOUS_OBJECT_BLANK
+            if !previous_object_id.blank?
+              previous_object = PREVIOUS_OBJECT_DIFFERENT
+              if attributes["id"] == previous_object_id
+                previous_object = PREVIOUS_OBJECT_SAME
+              end
+            end
+            
+            # Let's first check whether the non-id attributes are set or not
+            check_attributes = attributes.dup
+            check_attributes.delete_if {|innerkey, innervalue| innerkey == "id" }
 
-          non_id_attributes_set = false
-          if !model.attributes_blank?(attributes: check_attributes)
-            non_id_attributes_set = true
-          end
-          
-          Rails.logger.debug{"AllowExistingConcern.set_property_with_attributes object: #{attributes["id"]}, previous: #{previous_object_id}, previous_object: #{previous_object}, non_id_attributes_set: #{non_id_attributes_set}"}
-          
-          case update_type
-          when AllowExistingConcern::UPDATE_TYPE_UNKNOWN
-            # If non-id attributes are set, then we pretty much just discard whether or not the existing item is
-            # set and create a new item
-            if non_id_attributes_set
+            non_id_attributes_set = false
+            if !model.attributes_blank?(attributes: check_attributes)
+              non_id_attributes_set = true
+            end
+            
+            Rails.logger.debug{"AllowExistingConcern.set_property_with_attributes object: #{attributes["id"]}, previous: #{previous_object_id}, previous_object: #{previous_object}, non_id_attributes_set: #{non_id_attributes_set}"}
+            
+            case update_type
+            when AllowExistingConcern::UPDATE_TYPE_UNKNOWN
+              # If non-id attributes are set, then we pretty much just discard whether or not the existing item is
+              # set and create a new item
+              if non_id_attributes_set
+                attributes.delete_if {|innerkey, innervalue| innerkey == "id" }
+                Rails.logger.debug{"AllowExistingConcern.set_property_with_attributes creating new obj with attributes #{Myp.debug_print(attributes)}"}
+                newobj = Myp.new_model(model)
+                newobj.assign_attributes(attributes)
+                Rails.logger.debug{"AllowExistingConcern.set_property_with_attributes creating new obj #{Myp.debug_print(newobj)}"}
+                self.send("#{name}=", newobj)
+                attributes.clear
+              else
+                attributes.keep_if {|innerkey, innervalue| innerkey == "id" }
+                # Let's go find the existing object, authorize it, and set the property on this object
+                existing_obj = Myp.set_existing_object(self, name, model, attributes["id"].to_i, action: :show)
+                Rails.logger.debug{"AllowExistingConcern.set_property_with_attributes existing_obj: #{Myp.debug_print(existing_obj)}"}
+              end
+            when AllowExistingConcern::UPDATE_TYPE_COMBINE
+              existing_obj = Myp.set_existing_object(self, name, model, attributes["id"].to_i, action: :edit)
+              Rails.logger.debug{"AllowExistingConcern.set_property_with_attributes combining: #{Myp.debug_print(attributes)}"}
+
               attributes.delete_if {|innerkey, innervalue| innerkey == "id" }
-              Rails.logger.debug{"AllowExistingConcern.set_property_with_attributes creating new obj with attributes #{Myp.debug_print(attributes)}"}
-              newobj = Myp.new_model(model)
-              newobj.assign_attributes(attributes)
-              Rails.logger.debug{"AllowExistingConcern.set_property_with_attributes creating new obj #{Myp.debug_print(newobj)}"}
-              self.send("#{name}=", newobj)
+              existing_obj.assign_attributes(attributes)
               attributes.clear
-            else
-              attributes.keep_if {|innerkey, innervalue| innerkey == "id" }
-              # Let's go find the existing object, authorize it, and set the property on this object
-              existing_obj = Myp.set_existing_object(self, name, model, attributes["id"].to_i, action: :show)
-              Rails.logger.debug{"AllowExistingConcern.set_property_with_attributes existing_obj: #{Myp.debug_print(existing_obj)}"}
+
+              Rails.logger.debug{"AllowExistingConcern.set_property_with_attributes combined existing_obj: #{Myp.debug_print(existing_obj)}"}
             end
-          when AllowExistingConcern::UPDATE_TYPE_COMBINE
-            existing_obj = Myp.set_existing_object(self, name, model, attributes["id"].to_i, action: :edit)
-
-            attributes.delete_if {|innerkey, innervalue| innerkey == "id" }
-            existing_obj.assign_attributes(attributes)
+            
+            Rails.logger.debug{"AllowExistingConcern.set_property_with_attributes final attributes: #{attributes}"}
+          else
+            Rails.logger.debug{"AllowExistingConcern.set_property_with_attributes destroying name: #{name}"}
+            self.send("#{name}=", nil)
             attributes.clear
-
-            Rails.logger.debug{"AllowExistingConcern.set_property_with_attributes combined existing_obj: #{Myp.debug_print(existing_obj)}"}
           end
           
-          Rails.logger.debug{"AllowExistingConcern.set_property_with_attributes final attributes: #{attributes}"}
         else
           attributes.delete_if {|innerkey, innervalue| innerkey == "_updatetype" }
         end
