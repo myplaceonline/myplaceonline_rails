b5e13dfbe0e659d876388a74b8d6cd66ce5c450e
Kevin G <kevin@myplaceonline.com>
2019-04-11 15:28:04 -0700

    Suppress sensitive information in certain ActiveJobs

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -8,3 +8,18 @@ Delayed::Worker.read_ahead = 10
 #Delayed::Worker.delay_jobs = !Rails.env.test?
 Delayed::Worker.raise_signal_exceptions = :term
 #Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'))
+
+# https://stackoverflow.com/a/32807004/11030758
+# /usr/share/gems/gems/activejob-*/lib/active_job/logging.rb
+ActiveSupport.on_load :active_job do
+  class ActiveJob::Logging::LogSubscriber
+    private def args_info(job)
+      if job.arguments.any? && job.class.name != "UpdateUserPasswordJob" && job.class.name != "SwitchUserEncryptionJob"
+        " with arguments: " +
+          job.arguments.map { |arg| format(arg).inspect }.join(", ")
+      else
+        ""
+      end
+    end
+  end
+end
