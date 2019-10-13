5d90fab4fd08414d08cf6bfe60c3dc5308078035
Kevin G <kevin@myplaceonline.com>
2019-10-13 02:19:29 -0700

    Update sprocket config

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,11 @@
+// app/assets/config/manifest.js
+//
+//= link application.css
+//= link application.js
+//= jquery-mobile/icons-png/*.png
+//= jquery-mobile/ajax-loader.gif
+//= application_extra.js
+//= *.css
+//= ckeditor/*
+//= markerclusterer.js
+//= dygraph.js
