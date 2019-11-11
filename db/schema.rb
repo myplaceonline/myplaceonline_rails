1701c9e2e5da09e2e0e77b85b0540418ccaf7fcc
Kevin G <kevin@myplaceonline.com>
2019-11-10 18:13:42 -0800

    Add active storage migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_09_004718) do
+ActiveRecord::Schema.define(version: 2019_11_11_015138) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,27 @@ ActiveRecord::Schema.define(version: 2019_11_09_004718) do
     t.integer "rating"
     t.boolean "is_public"
     t.index ["identity_id"], name: "index_acne_measurements_on_identity_id"
+  end
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
   create_table "activities", id: :serial, force: :cascade do |t|
@@ -2071,6 +2092,7 @@ ActiveRecord::Schema.define(version: 2019_11_09_004718) do
     t.boolean "hidden"
     t.integer "created_by"
     t.integer "dealbreaker_type"
+    t.boolean "controversial"
     t.index ["identity_id"], name: "index_drom_match_dealbreakers_on_identity_id"
   end
 
@@ -2082,6 +2104,7 @@ ActiveRecord::Schema.define(version: 2019_11_09_004718) do
     t.datetime "updated_at", null: false
     t.boolean "setting_explicit"
     t.boolean "asked_name"
+    t.boolean "setting_controversial"
     t.index ["identity_id"], name: "index_drom_match_identity_infos_on_identity_id"
   end
 
@@ -7475,6 +7498,7 @@ ActiveRecord::Schema.define(version: 2019_11_09_004718) do
   add_foreign_key "acne_measurement_pictures", "identities", name: "acne_measurement_pictures_identity_id_fk"
   add_foreign_key "acne_measurement_pictures", "identity_files", name: "acne_measurement_pictures_identity_file_id_fk"
   add_foreign_key "acne_measurements", "identities", name: "acne_measurements_identity_id_fk"
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "activities", "identities", name: "activities_identity_id_fk"
   add_foreign_key "admin_emails", "emails"
   add_foreign_key "admin_emails", "identities"
