955fe91322b32bafe107e5c7c026377d925b0839
Kevin G <kevin@myplaceonline.com>
2021-05-02 14:49:51 -0700

    Upgrade to Rails6

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -9,7 +9,7 @@ class Users::SessionsController < Devise::SessionsController
     self.resource.remember_me = true
     @redirect = nil
     if !params[:redirect].blank?
-      @redirect = URI.parse(URI.decode(params[:redirect])).to_s
+      @redirect = URI.parse(CGI.unescape(params[:redirect])).to_s
     end
     clean_up_passwords(resource)
     respond_with(resource, serialize_options(resource))
@@ -41,7 +41,7 @@ class Users::SessionsController < Devise::SessionsController
     if params[:redirect].blank?
       logged_in_path = after_sign_in_path_for(resource)
     else
-      logged_in_path = URI.parse(URI.decode(params[:redirect])).to_s
+      logged_in_path = URI.parse(CGI.unescape(params[:redirect])).to_s
     end
     respond_with resource, location: logged_in_path
   end
@@ -53,7 +53,7 @@ class Users::SessionsController < Devise::SessionsController
     
     @redirect = nil
     if params.has_key?(:redirect)
-      @redirect = URI.parse(URI.decode(params[:redirect].scrub)).to_s
+      @redirect = URI.parse(CGI.unescape(params[:redirect].scrub)).to_s
     end
     
     if !session[:password].blank? && current_user.valid_password?(session[:password])
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,8 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
+  
+  config.hosts = nil
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
