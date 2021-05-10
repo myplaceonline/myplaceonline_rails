1569f2049446672fd406c9affcd3809bd07c32a8
Kevin G <kevin@myplaceonline.com>
2021-05-09 19:08:51 -0700

    Fix production issue on Rails 6.1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,6 +9,24 @@ class DynamicDefaultUrlOptions
   end
 end
 
+class DynamicSmtpSettings
+  def initialize(params = {})
+    @params = params.merge({
+      address: ENV["SMTP_HOST"],
+      port: 587,
+      user_name: ENV["SMTP_USER"],
+      password: ENV["SMTP_PASSWORD"],
+      authentication: :login,
+      enable_starttls_auto: true,
+    })
+  end
+  
+  def method_missing(name, *args, &block)
+    @params[:domain] = MyplaceonlineExecutionContext.host
+    @params.send(name, *args, &block)
+  end
+end
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
