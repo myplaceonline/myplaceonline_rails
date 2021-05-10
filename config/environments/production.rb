ee4e751af421aab752be50d24dd2e97951f87d12
Kevin G <kevin@myplaceonline.com>
2021-05-09 19:04:47 -0700

    Fix production issue on Rails 6.1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,14 @@
+class DynamicDefaultUrlOptions
+  def initialize(params = {})
+    @params = params
+  end
+  
+  def method_missing(name, *args, &block)
+    @params[:host] = MyplaceonlineExecutionContext.host
+    @params.send(name, *args, &block)
+  end
+end
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
