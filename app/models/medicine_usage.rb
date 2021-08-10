8e0015c27f5149c6f945a83434bc70f7c15025d4
Kevin G <kevin@myplaceonline.com>
2021-08-09 18:48:01 -0700

    Add year to medicine usage display

--- app/models/medicine_usage.rb
+++ app/models/medicine_usage.rb
@@ -7,9 +7,9 @@ class MedicineUsage < ApplicationRecord
   child_properties(name: :medicine_usage_medicines)
   
   def display
-    result = Myp.display_datetime_short(usage_time, User.current_user)
+    result = Myp.display_datetime_short_year(usage_time, User.current_user)
     if !self.usage_end.nil?
-        result += " - " + Myp.display_datetime_short(self.usage_end, User.current_user)
+        result += " - " + Myp.display_datetime_short_year(self.usage_end, User.current_user)
     end
     result = Myp.appendstrwrap(result, self.description)
     return result
