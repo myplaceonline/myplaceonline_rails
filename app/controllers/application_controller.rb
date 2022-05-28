59f98ccee0506188fbb92b3da9a728dec5e91436
Kevin G <kevin@myplaceonline.com>
2022-05-28 12:03:56 -0500

    Update gems and add rack-cors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,6 +21,15 @@ class ApplicationController < ActionController::Base
   check_authorization
   
   rescue_from StandardError, :with => :catchall
+
+  after_action :cors_set_access_control_headers
+
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    #headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    #headers['Access-Control-Allow-Headers'] = '*'
+    #headers['Access-Control-Max-Age'] = "1728000"
+  end
   
   #utf8_enforcer_workaround
   
