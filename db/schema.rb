736fff5a04793f5c4b9c7f5f54b14d564d9e33f8
Kevin G <kevin@myplaceonline.com>
2022-10-08 08:03:29 -0500

    Update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_06_015451) do
+ActiveRecord::Schema.define(version: 2022_10_08_125565) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -3066,6 +3066,10 @@ ActiveRecord::Schema.define(version: 2022_10_06_015451) do
     t.boolean "lamb"
     t.boolean "veal"
     t.boolean "venison"
+    t.boolean "certified_humane_free_range"
+    t.boolean "certified_humane_pasture_raised_grass_fed"
+    t.boolean "supermarket"
+    t.boolean "restaurant"
     t.index ["identity_id"], name: "index_find_humane_humane_products_on_identity_id"
   end
 
