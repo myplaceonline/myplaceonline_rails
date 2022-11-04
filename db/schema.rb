7336e143ed413c20b4c01b66ce4f6dd6a08fd039
Kevin G <kevin@myplaceonline.com>
2022-11-03 20:26:09 -0500

    Update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_28_025728) do
+ActiveRecord::Schema.define(version: 2022_11_04_004843) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -3085,6 +3085,11 @@ ActiveRecord::Schema.define(version: 2022_10_28_025728) do
     t.boolean "paleofriendly"
     t.boolean "dairyfree"
     t.boolean "nongmo"
+    t.boolean "american_humane_certified"
+    t.boolean "american_grassfed_association"
+    t.boolean "usda_organic"
+    t.boolean "food_alliance_certified"
+    t.boolean "one_health_certified"
     t.index ["identity_id"], name: "index_find_humane_humane_products_on_identity_id"
   end
 
