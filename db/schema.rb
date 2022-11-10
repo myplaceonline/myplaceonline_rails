5ea05aee802dcdb977e060feed541a5e8fef0b1b
Kevin G <kevin@myplaceonline.com>
2022-11-10 12:21:50 -0600

    Update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_08_022136) do
+ActiveRecord::Schema.define(version: 2022_11_10_181567) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -3122,6 +3122,7 @@ ActiveRecord::Schema.define(version: 2022_11_08_022136) do
     t.boolean "animal_welfare_certified_fish_1"
     t.boolean "animal_welfare_certified_fish_3"
     t.boolean "animal_welfare_certified_fish_6"
+    t.boolean "kosher"
     t.index ["identity_id"], name: "index_find_humane_humane_products_on_identity_id"
   end
 
