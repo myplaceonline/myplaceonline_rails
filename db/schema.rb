d7466ee5eae4960971c0cc98ae1436a769c5a611
Kevin G <kevin@myplaceonline.com>
2022-11-17 20:31:29 -0600

    Update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_16_224364) do
+ActiveRecord::Schema.define(version: 2022_11_18_015971) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -3130,6 +3130,11 @@ ActiveRecord::Schema.define(version: 2022_11_16_224364) do
     t.boolean "noartificial"
     t.boolean "nocrates"
     t.boolean "nongmoproject"
+    t.integer "cornucopia_institute_score"
+    t.integer "cornucopia_institute_score_type"
+    t.integer "cornucopia_institute_rating"
+    t.boolean "certified_paleo"
+    t.boolean "keto_certified"
     t.index ["identity_id"], name: "index_find_humane_humane_products_on_identity_id"
   end
 
