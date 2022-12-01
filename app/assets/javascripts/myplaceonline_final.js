564d6192dadd2403fe8eea7032526d1c484c5596
Kevin G <kevin@myplaceonline.com>
2022-12-01 17:34:06 -0600

    Comment out

--- app/assets/javascripts/myplaceonline_final.js
+++ app/assets/javascripts/myplaceonline_final.js
@@ -142,6 +142,7 @@ var myplaceonline = function(mymodule) {
     }
   });
 
+  /*
   $(document).on("click", "a", function(e) {
     if ($(this).attr('rel') == "external") {
       e.preventDefault();
@@ -155,6 +156,7 @@ var myplaceonline = function(mymodule) {
       return false;
     }
   });
+  */
   
   function jqmSetListMessage(list, message) {
     list.html("<li data-role=\"visible\">" + message + "</li>");
