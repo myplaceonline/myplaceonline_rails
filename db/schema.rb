8e890663e99f38e0f7adf36c8051438bd8fff489
Kevin G <kevin@myplaceonline.com>
2022-12-30 15:43:20 -0600

    Update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_23_002534) do
+ActiveRecord::Schema.define(version: 2022_12_30_213958) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -2973,6 +2973,7 @@ ActiveRecord::Schema.define(version: 2022_12_23_002534) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "noutm"
+    t.boolean "delivery"
     t.index ["identity_id"], name: "index_find_humane_ads_on_identity_id"
   end
 
