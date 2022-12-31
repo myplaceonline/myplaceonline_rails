ccc94a9b238aa20fb058150d2b1ec48048a26feb
Kevin G <kevin@myplaceonline.com>
2022-12-31 09:22:37 -0600

    Update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_30_213958) do
+ActiveRecord::Schema.define(version: 2022_12_31_151878) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -3047,6 +3047,7 @@ ActiveRecord::Schema.define(version: 2022_12_30_213958) do
     t.boolean "onlineonly"
     t.decimal "score", precision: 10, scale: 2
     t.string "website_underlying"
+    t.boolean "linknoutm"
     t.index ["identity_id"], name: "index_find_humane_humane_locations_on_identity_id"
     t.index ["location_id"], name: "index_find_humane_humane_locations_on_location_id"
     t.index ["parent_location_id"], name: "index_find_humane_humane_locations_on_parent_location_id"
