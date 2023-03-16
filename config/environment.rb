99c104382186fa7e3045a04656aec45eedfe4242
Kevin G <kevin@myplaceonline.com>
2023-03-16 14:55:23 -0500

    Update gems

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,19 @@
 # Load the Rails application.
 require_relative 'application'
 
+if !ENV["MEMPROFILE"].blank?
+  
+  # This profiles all initializers including engines
+
+  require 'memory_profiler'
+  MemoryProfiler.start
+end
+
 # Initialize the Rails application.
 Rails.application.initialize!
+
+if !ENV["MEMPROFILE"].blank?
+  puts "MEMPROFILE: config/environment"
+  report = MemoryProfiler.stop
+  report.pretty_print
+end
