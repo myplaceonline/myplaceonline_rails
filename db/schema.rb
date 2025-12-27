312473b7fde1c7a20b0ebdc916d58523aaf83ca8
Kevin G <kevin@myplaceonline.com>
2025-12-26 22:15:07 -0600

    Updates

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_12_27_034457) do
+ActiveRecord::Schema.define(version: 2025_12_27_041661) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -6510,6 +6510,7 @@ ActiveRecord::Schema.define(version: 2025_12_27_034457) do
     t.boolean "is_public"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "approved"
     t.index ["identity_id"], name: "index_rabbl_communities_on_identity_id"
   end
 
